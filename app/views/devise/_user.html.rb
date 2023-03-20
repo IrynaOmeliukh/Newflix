6dbc5cb8bb4cc2e5159ed2120ef30ff830898f8a
Iryna Omeliukh <iryna.omeliukh@gmail.com>
2023-03-20 11:28:16 +0200

    user views

--- app/views/devise/_user.html.rb
+++
@@ -1,7 +0,0 @@
-<tr>
-          <td class="border px-4 py-2 text-center"><%= user.nickname %></td>
-          <td class="border px-4 py-2 text-center"><%= user.email %></td>
-          <td class="border px-4 py-2 text-center">
-            <a href="<%= user_path(user) %>" class="text-blue-500 hover:text-blue-700">View User</a>
-          </td>
-        </tr>
